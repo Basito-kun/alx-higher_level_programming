adb94d0ebb87921daee803cfd28bcbd471cf55f1
Basito-kun <abdulbasit.oa7@gmail.com>
2024-06-24 03:37:17 -0700

    Corrections on printing the last digit of a number stored in a variable number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-saigo = abs(number) % 10
-if saigo > 5:
-    print(f"Last digit of {number} is {saigo} and is greater than 5")
-elif saigo == 0:
-    print(f"Last digit of {number} is {saigo} and is 0")
+# To ensure the last digit of a -ive result is also -ive
+if number < 0:
+    saigo = -(-number % 10)
 else:
-    print(f"Last digit of {number} is {saigo} and is less than 6 and not 0")
+    saigo = number % 10
+print(f"Last digit of {number} is {saigo}", end=" ")
+if saigo > 5:
+    print("and is greater than 5")
+elif saigo == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
