026f413a98b6c238132451c085c26bcba6ab285c
Basito-kun <abdulbasit.oa7@gmail.com>
2024-07-01 06:45:50 -0700

    Corrections to Task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for suji in range(0, 99):   # to get the nos in dec
-    print("{} = ".format(suji), end="")
-    suji = hex(suji)    # to get the nos in hex
-    print("{}".format(suji))    # I DONT GET WHY '0x' IS AUTO PRINTED THO!!!
+    print("{:d} = 0x{:x}".format(suji, suji))
+    # :d is to decimal and :x is to hexadeximal.
